a0aece80bc2d5a01765aae49ff1040c81bff0bef
Ravindu-Hasanka <rhatu2000@gmail.com>
2025-08-27 10:47:50 +0530

    feat: recommended courses endpoint

--- src/main/java/com/example/pathwisebackend/Controllers/SkillsController.java
+++ src/main/java/com/example/pathwisebackend/Controllers/SkillsController.java
@@ -1,25 +1,21 @@
 package com.example.pathwisebackend.Controllers;
 
 import com.example.pathwisebackend.Services.SkillsService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping(name = "/skills")
+@RequestMapping("/api/skills")
+@RequiredArgsConstructor
+@CrossOrigin
 public class SkillsController {
     private final SkillsService skillsService;
 
-    public SkillsController(SkillsService skillsService) {
-        this.skillsService = skillsService;
-    }
-
     @GetMapping("/recommended-resources/{id}")
-    public List<Map<String, Object>> getRecommendedResources(@RequestParam Long id) {
+    public List<Map<String, Object>> getRecommendedResources(@PathVariable("id") Long id) {
         return skillsService.getRecommendedResources(id);
     }
 }
