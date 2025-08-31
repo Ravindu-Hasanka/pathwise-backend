17529af185aa5f3af2d70a7a68aad19510d39307
Siluni <wijesinghe.anj@gmail.com>
2025-08-31 14:38:59 +0530

    feat:user suggestion api

--- src/main/java/com/example/pathwisebackend/Services/NetworkServiceImpl.java
+++ src/main/java/com/example/pathwisebackend/Services/NetworkServiceImpl.java
@@ -1,9 +1,10 @@
 package com.example.pathwisebackend.Services;
 
 import com.example.pathwisebackend.DTO.ConnectionDTO;
+import com.example.pathwisebackend.DTO.User.UserDTO;
+import com.example.pathwisebackend.Enum.ConnectionStatus;
 import com.example.pathwisebackend.Interfaces.INetworkService;
 import com.example.pathwisebackend.Models.Connection;
-import com.example.pathwisebackend.Enum.ConnectionStatus;
 import com.example.pathwisebackend.Models.User;
 import com.example.pathwisebackend.Repositories.ConnectionRepository;
 import com.example.pathwisebackend.Repositories.UserRepository;
@@ -11,7 +12,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -85,6 +88,47 @@ public class NetworkServiceImpl implements INetworkService {
                     dto.setJobRole(requester.getRole().toString());
                     dto.setEmail(requester.getEmail());
                     dto.setRequestedAt(c.getRequestedAt());
+                    return dto;
+                })
+                .toList();
+    }
+    public List<ConnectionDTO> getSentRequests(Long userId) {
+
+        List<Connection> requests= connRepo.findSentRequests(userId);
+        return requests.stream()
+                .map(c -> {
+                    User receiver = c.getReceiver();
+                    ConnectionDTO dto = new ConnectionDTO();
+                    dto.setConnectionId(c.getConnectionId());
+                    dto.setRequestedUserId(receiver.getId());
+                    dto.setRequestedUserName(receiver.getName());
+                    dto.setJobRole(receiver.getRole().toString());
+                    dto.setEmail(receiver.getEmail());
+                    dto.setRequestedAt(c.getRequestedAt());
+                    return dto;
+                })
+                .toList();
+    }
+    public List<UserDTO> getSuggestions(Long userId) {
+        // get all existing connections (pending + accepted)
+        List<Connection> userConnections = connRepo.findByUser(userId);
+        Set<Long> excludedIds = new HashSet<>();
+        excludedIds.add(userId);
+
+        for (Connection c : userConnections) {
+            excludedIds.add(c.getInitiator().getId());
+            excludedIds.add(c.getReceiver().getId());
+        }
+
+        // return users not in excludedIds
+        return userRepo.findAll().stream()
+                .filter(u -> !excludedIds.contains(u.getId())  )
+                .map(u -> {
+                    UserDTO dto = new UserDTO();
+                    dto.setUserId(u.getId());
+                    dto.setName(u.getName());
+                    dto.setEmail(u.getEmail());
+                    dto.setRole(u.getRole().toString());
                     return dto;
                 })
                 .toList();
